5e4d463ee61024632dedffbfc83147985a3fde4b
Anton <manton2007@gmail.com>
2022-11-16 16:40:21 +0200

    some camera work

--- src/Components/Camera.svelte
+++ src/Components/Camera.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
     import {PerspectiveCamera, Vector3} from "svelthree";
-
-    export let scene;
+    import PerspectiveCameraRemake from "./PerspectiveCameraRemake.svelte";
 
     let cameraPos: Vector3 = new Vector3(0, 0, 3);
     let cameraLook: Vector3 = new Vector3(0, 0, 0);
-    setInterval(()=>{cameraPos = new Vector3(0, 0, cameraPos.z - 0.1)}, 100);
+
+
+    let camera: PerspectiveCamera;
+    export let scene;
+
+
+
+    setInterval(()=>{camera.rotation.x = 6.28}, 10);
+    function handleMouseMove(e){
+
+    }
 </script>
 
 
-<PerspectiveCamera {scene} id="main_camera" pos={cameraPos} lookAt={cameraLook}/>
+<PerspectiveCameraRemake on:mousemove={handleMouseMove} bind:cam={camera} {scene} id="main_camera" pos={cameraPos} lookAt={cameraLook}/>
