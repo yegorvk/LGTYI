8b6522dead05b8355953359f2dcf69b60f9ce1a3
Yegor Vaskonyan <egor.vaskoyan@gmail.com>
2022-12-20 21:47:17 +0100

    Add complete camera support for 2d view

--- src/Components/Renderer2D.svelte
+++ src/Components/Renderer2D.svelte
@@ -1,43 +1,154 @@
 <script lang="ts">
     import type {Heightmap} from "../Terrain/Heightmap";
+    import * as THREE from 'three'
     import {onMount} from "svelte";
     import {colorGrayScaleFromAltitude, rgbFromGrayScale} from "../Terrain/PointColor";
 
+    // bindings
     let canvas: HTMLCanvasElement;
+
+    let canvasContext = null
+
     export let data: Heightmap;
     export let d2Scale: number = 5;
-    onMount(()=>{
-        let context: CanvasRenderingContext2D = canvas.getContext('2d');
-        canvas.onwheel = zoom;
-        for (let i=0; i < data.size; i++){
-            for (let j = 0; j < data.size; j++){
-                let id = j + i * data.size;
-                let grayscale = colorGrayScaleFromAltitude(data.data[id]);
-                let rgb = rgbFromGrayScale(grayscale)
-                context.fillStyle = `#${rgb.toString(16)}`;
-                context.fillRect(i*d2Scale, j*d2Scale, d2Scale, d2Scale);
-            }
+
+    let scale = 1.0;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    const pixels = new Uint8ClampedArray(4*data.size*data.size)
+
+    for (let i = 0; i < data.size; i++) {
+        for (let j = 0; j < data.size; j++) {
+            const grayscale = colorGrayScaleFromAltitude(
+                data.data[i*data.size+j]
+            )
+
+            const base = 4*(i*data.size+j)
+ 
+            pixels[base] = pixels[base+1] = pixels[base+2] = grayscale 
+            pixels[base+3] = 255
         }
-    });
-    let scale = 1;
-    function zoom(event) {
-        event.preventDefault();
-
-        scale += event.deltaY * -0.001;
-
-        // Restrict scale
-        scale = Math.min(Math.max(.125, scale), 8);
-
-        // Apply scale transform
-        canvas.style.transform = `scale(${scale})`;
     }
 
+    const imgData = new ImageData(pixels, data.size, data.size)
+    let imgBmp: ImageBitmap = null
+    
+    createImageBitmap(
+        imgData
+    ).then(
+        (bmp) => {
+            imgBmp = bmp
+            draw(canvasContext)
+        },
+        (reason) => {
+            console.error(reason)
+        }
+    )
+
+    console.log(imgData)
+
+    function draw(ctx: CanvasRenderingContext2D) {
+        if (imgBmp === null || ctx === null) return;
+
+        ctx.resetTransform()
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+        ctx.translate(
+            -offsetX + canvas.width * (1 - scale) / 2,
+            -offsetY + canvas.height * (1 - scale) / 2
+        )
+
+        ctx.scale(scale, scale)
+
+        ctx.drawImage(imgBmp, 0, 0, canvas.width, canvas.height);
+    }
+
+    function fixOffsets() {
+        const contentWidth = scale * canvas.width;
+        const contentHeight = scale * canvas.height;
+
+        const dWidth = Math.max(0, contentWidth - canvas.width)
+        const dHeight = Math.max(0, contentHeight - canvas.height)
+
+        offsetX = Math.max(offsetX, -dWidth / 2)
+        offsetX = Math.min(offsetX, dWidth / 2)
+
+        offsetY = Math.max(offsetY, -dHeight / 2)
+        offsetY = Math.min(offsetY, dHeight / 2)
+    }
+
+    onMount(() => {
+        canvasContext = canvas.getContext('2d');
+
+        canvas.onwheel = (event: WheelEvent) => {
+            event.preventDefault()
+
+            fixOffsets()
+
+            const delta = event.deltaY * scale * 0.001
+            const oldScale = scale
+
+            scale -= delta
+            scale = Math.max(0.125, Math.min(8.0, scale));
+
+            offsetX *= (scale / oldScale)
+            offsetY *= (scale / oldScale)
+
+            fixOffsets()
+
+            draw(canvasContext)
+        };
+
+        function resizeCanvas() {
+            canvas.height = window.innerHeight - 8;
+            canvas.width = canvas.height;
+        }
+
+        window.onresize = () => {
+            resizeCanvas()
+            draw(canvasContext)
+        }
+
+        resizeCanvas()
+
+        let pointerDown = false
+        let lastX = 0, lastY = 0
+
+        document.onpointerdown = beginDrag
+        document.onpointermove = drag
+        document.onpointerup = document.onpointercancel = endDrag
+
+        function beginDrag(event: PointerEvent) {
+            if (event.isPrimary && !pointerDown) {
+                pointerDown = true
+                lastX = event.x, lastY = event.y
+            }
+        }
+
+        function drag(event: PointerEvent) {
+            if (event.isPrimary && pointerDown) {
+                const deltaX = event.x - lastX
+                const deltaY = event.y - lastY
+
+                offsetX -= deltaX * 0.02
+                offsetY -= deltaY * 0.02
+
+                fixOffsets()
+
+                draw(canvasContext)
+            }
+        }
+
+        function endDrag(event: PointerEvent) {
+            if (event.isPrimary && pointerDown) {
+                pointerDown = false
+            }
+        }
+
+        draw(canvasContext)
+    });
 </script>
 
 <canvas id="rendererD2"
-        bind:this={canvas}
-        width={data.size*d2Scale}
-        height={data.size*d2Scale}
->
-
-</canvas>
+        bind:this={canvas}/>
