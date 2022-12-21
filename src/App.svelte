2976b6aff6ccb3b21b2725df48cd618727bc6b91
Anton <manton2007@gmail.com>
2022-12-21 10:42:45 +0200

    add substr func

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,7 @@
     import {importMap} from "./Files/Importer";
     import Notification from "./Components/Notification/Notification.svelte";
     import AddMap from "./Files/Adder";
+    import SubtructMap from "./Files/Subtructer";
 
     let trigger: boolean;
     let is2DView: boolean = false;
@@ -44,6 +45,18 @@
         }
 
     }
+    async function substrMap(){
+        invisible = true;
+        try {
+            heightmap = await SubtructMap(heightmap);
+            notify("Successfully subtracted!", false);
+            heightmap = heightmap;
+            invisible = false;
+        }catch (err){
+            invisible = false;
+            notify(err, true);
+        }
+    }
     function ExportMap(){
         exportMap(heightmap).then(()=>{notify("Successfully exported!", false);}).catch(err=>{notify(err, true)});
     }
@@ -52,7 +65,7 @@
 
 <main id="app_content">
     <Notification bind:notify={notify}></Notification>
-    <UI {generate} bind:is2D={is2DView} on:export_map={ExportMap} on:import_map={ImportMap} on:add_map={addMap}/>
+    <UI {generate} bind:is2D={is2DView} on:export_map={ExportMap} on:import_map={ImportMap} on:add_map={addMap} on:substr_map={substrMap}/>
     {#if !invisible}
     {#if !is2DView}
         {#key trigger}
--- src/Files/Subtructer.ts
+++ src/Files/Subtructer.ts
@@ -1,6 +1,28 @@
-import {Heightmap} from "../Terrain/Heightmap";
+import type {Heightmap} from "../Terrain/Heightmap";
+import {importMap} from "./Importer";
 
-export default function SubtructMap(heightMap: Heightmap): Heightmap{
+export default async function SubtructMap(heightMap: Heightmap): Promise<Heightmap>{
+    let importedHeightmap;
+    try {
+        importedHeightmap = await importMap()
+    } catch (err) {
+        throw  new Error(err);
+    }
+    let dif = (Math.max(heightMap.size, importedHeightmap.size) - Math.min(heightMap.size, importedHeightmap.size)) / 2;
+    if (heightMap.size < importedHeightmap.size) {
+        for (let i = 0; i < heightMap.size; i++) {
+            for (let j = 0; j < heightMap.size; j++) {
+                heightMap.data[j] -= importedHeightmap.data[j + dif + dif * importedHeightmap.size + i * (heightMap.size + 2 * dif)];
+            }
+        }
+        return importedHeightmap;
+    } else {
+        for (let i = 0; i < importedHeightmap.size; i++) {
+            for (let j = 0; j < importedHeightmap.size; j++) {
+                heightMap.data[j + dif + dif * heightMap.size + i * (importedHeightmap.size + 2 * dif)] -= importedHeightmap.data[j];
+            }
+        }
+        return heightMap;
+    }
 
-    return new Heightmap();
 }
