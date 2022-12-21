49402fb0fcb8cb721b8fa0ea8108340f32639765
Anton <manton2007@gmail.com>
2022-12-21 11:04:25 +0200

    adaptation of add, substr, import, export

--- src/Files/Importer.ts
+++ src/Files/Importer.ts
@@ -2,9 +2,10 @@ import {Heightmap} from "../Terrain/Heightmap";
 const remote = require('@electron/remote');
 
 
-export async function importMap() {
+export async function importMap(): Promise<Heightmap> {
     interface readData{
-        size: number,
+        width: number,
+        height: number,
         data: Float32Array,
         offsetX: number,
         offsetY: number
@@ -23,15 +24,15 @@ export async function importMap() {
 
         let Data: readData = JSON.parse(file);
 
-        if (!(Data.offsetX === undefined || Data.offsetY === undefined || Data.size === undefined || Data.data === undefined )) {
-            let arr = new Float32Array(Data.size*Data.size);
+        if (!(Data.offsetX === undefined || Data.offsetY === undefined || Data.width === undefined || Data.height === undefined || Data.data === undefined )) {
+            let arr = new Float32Array(Data.width*Data.height);
             let i = 0;
             for(let key of Object.values(Data.data)) {
                 arr[i] = key;
                 i++;
             }
             Data.data = arr;
-            return new Heightmap(Data.size, Data.data, Data.offsetX, Data.offsetY);
+            return new Heightmap(Data.width, Data.height, Data.data, Data.offsetX, Data.offsetY);
         }else {
             throw new Error('Invalid Data');
         }
