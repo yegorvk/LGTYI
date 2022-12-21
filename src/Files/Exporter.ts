ff2b612a279c8d6d76474d0351a12eb7a2c51cbf
Anton <manton2007@gmail.com>
2022-12-21 11:15:46 +0200

    refactoring

--- src/Files/Exporter.ts
+++ src/Files/Exporter.ts
@@ -1,25 +1,26 @@
 import type {Heightmap} from "../Terrain/Heightmap";
+
 const remote = require('@electron/remote');
 
 export async function exportMap(heightMap: Heightmap) {
-        const fs = window.require('fs');
-        // Use the electron dialog module to prompt the user for a file path
-        const {filePath} = await remote.dialog.showSaveDialog({
-            filters: [{name: 'JSON Files', extensions: ['json']}]
-        });
+    const fs = window.require('fs');
+    // Use the electron dialog module to prompt the user for a file path
+    const {filePath} = await remote.dialog.showSaveDialog({
+        filters: [{name: 'JSON Files', extensions: ['json']}]
+    });
 
-        // Exit the function if the user cancels the save dialog
-        if (!filePath) throw new Error('No file selected');
+    // Exit the function if the user cancels the save dialog
+    if (!filePath) throw new Error('No file selected');
 
-        // Use the electron fs module to write an empty string to the specified file
-        fs.writeFile(filePath, JSON.stringify(heightMap, null, 2), 'utf8', (error) => {
-            if (error) {
-                // Handle the error
-                throw new Error(error);
-            } else {
-                // The file was saved successfully
-                return;
-            }
-        });
+    // Use the electron fs module to write an empty string to the specified file
+    fs.writeFile(filePath, JSON.stringify(heightMap, null, 2), 'utf8', (error) => {
+        if (error) {
+            // Handle the error
+            throw new Error(error);
+        } else {
+            // The file was saved successfully
+            return;
+        }
+    });
 
 }
--- src/Files/Importer.ts
+++ src/Files/Importer.ts
@@ -1,15 +1,17 @@
 import {Heightmap} from "../Terrain/Heightmap";
+
 const remote = require('@electron/remote');
 
 
 export async function importMap(): Promise<Heightmap> {
-    interface readData{
+    interface readData {
         width: number,
         height: number,
         data: Float32Array,
         offsetX: number,
         offsetY: number
     }
+
     const fs = window.require('fs');
     // Use the electron dialog module to prompt the user for a file path
     const {filePaths} = await remote.dialog.showOpenDialog({
@@ -20,25 +22,25 @@ export async function importMap(): Promise<Heightmap> {
     if (!filePaths) return;
     // Use the electron fs module to write an empty string to the specified file
     try {
-        let file = await fs.promises.readFile(filePaths[0],'utf8');
+        let file = await fs.promises.readFile(filePaths[0], 'utf8');
 
         let Data: readData = JSON.parse(file);
 
-        if (!(Data.offsetX === undefined || Data.offsetY === undefined || Data.width === undefined || Data.height === undefined || Data.data === undefined )) {
-            let arr = new Float32Array(Data.width*Data.height);
+        if (!(Data.offsetX === undefined || Data.offsetY === undefined || Data.width === undefined || Data.height === undefined || Data.data === undefined)) {
+            let arr = new Float32Array(Data.width * Data.height);
             let i = 0;
-            for(let key of Object.values(Data.data)) {
+            for (let key of Object.values(Data.data)) {
                 arr[i] = key;
                 i++;
             }
             Data.data = arr;
             return new Heightmap(Data.width, Data.height, Data.data, Data.offsetX, Data.offsetY);
-        }else {
+        } else {
             throw new Error('Invalid Data');
         }
 
-    }catch (err){
-       throw new Error(err);
+    } catch (err) {
+        throw new Error(err);
     }
 
 }
