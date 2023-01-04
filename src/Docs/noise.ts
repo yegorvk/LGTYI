d7858112c08c8d829262205fd51f240459291fd8
Anton <manton2007@gmail.com>
2023-01-04 23:14:48 +0200

    Update Docs

--- src/Docs/noise.ts
+++ src/Docs/noise.ts
@@ -1,6 +1,18 @@
 /**
- * PerlinNoise category.
- * PerlinNoise is a noise used in project to generate the map.
+ * __Translated to typescript
+ * Original comment:__
+ * `A speed-improved perlin and simplex noise algorithms for 2D.
+ *
+ * Based on example code by Stefan Gustavson (stegu@itn.liu.se).
+ * Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
+ * Better rank ordering method by Stefan Gustavson in 2012.
+ * Converted to Javascript by Joseph Gentle.
+ *
+ * Version 2012-03-09
+ *
+ * This code was placed in the public domain by its original author,
+ * Stefan Gustavson. You may use it as you see fit, but
+ * attribution is appreciated.`
  * @module
  */
 export * from "../PerlinNoise/PerlinNoise";
--- src/PerlinNoise/PerlinNoise.ts
+++ src/PerlinNoise/PerlinNoise.ts
@@ -1,23 +1,5 @@
 
 /**
- * __Translated to typescript
- * Original comment:__
- * `A speed-improved perlin and simplex noise algorithms for 2D.
- *
- * Based on example code by Stefan Gustavson (stegu@itn.liu.se).
- * Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
- * Better rank ordering method by Stefan Gustavson in 2012.
- * Converted to Javascript by Joseph Gentle.
- *
- * Version 2012-03-09
- *
- * This code was placed in the public domain by its original author,
- * Stefan Gustavson. You may use it as you see fit, but
- * attribution is appreciated.`
- * @module
- */
-
-/**
  * It is just a Vector
  * */
 export class Grad {
