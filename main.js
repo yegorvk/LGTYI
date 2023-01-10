71a4c739d33067e03b1603ababafb2b36f49bdd8
Anton <manton2007@gmail.com>
2023-01-10 21:46:08 +0200

    fix safe enviroment

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function createWindow() {
             nodeIntegration: true,
             contextIsolation: false,
             enableRemoteModule: true,
-            preload: path.join(__dirname, 'src/preload.js'),
+            preload: path.join(__dirname, 'preload.js'),
         },
         icon: path.join(__dirname, 'public/favicon.png'),
         show: false
